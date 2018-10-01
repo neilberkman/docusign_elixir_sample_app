0ac06dccdcc8ab7515629a0a40845777ece71917
Pavel Herasimau <pavelgnom@gmail.com>
2018-10-01 17:20:05 +0200

    sending pdf, docs and html files to docusign

--- lib/docusign_elixir_sample_app.ex
+++ lib/docusign_elixir_sample_app.ex
@@ -25,6 +25,43 @@ defmodule DocusignElixirSampleApp do
     end
   end
 
+  @doc """
+  Sending a file to Docusign.
+  """
+  @spec send_envelop() :: {:ok, DocuSign.Model.EnvelopeDefinition.t()} | {:error, binary}
+  def send_envelop do
+    Logger.debug("Preparing envelopes...")
+
+    documents =
+      Enum.map(~w(docx html pdf), fn ext ->
+        %DocuSign.Model.Document{
+          documentBase64: Base.encode64(File.read!("priv/samples/sample.#{ext}")),
+          name: "elixir.#{ext}",
+          fileExtension: ext,
+          documentId: "elixir-#{ext}-#{Timex.to_unix(Timex.now())}"
+        }
+      end)
+
+    definition = %DocuSign.Model.EnvelopeDefinition{
+      emailSubject: "Please sign this documents sent from Elixir SDK",
+      documents: documents
+    }
+
+    Logger.debug("Sending envelopes...")
+
+    case Api.Envelopes.envelopes_post_envelopes(connection(), account_id(),
+           envelopeDefinition: definition
+         ) do
+      {:ok, %DocuSign.Model.EnvelopeDefinition{} = envelope_definition} ->
+        Logger.debug("Envelopes has been sent.")
+        envelope_definition
+
+      {:error, %Tesla.Env{body: error}} ->
+        Logger.error(inspect(error))
+        {:error, error}
+    end
+  end
+
   defp connection, do: DocuSign.Connection.new(client: DocuSign.APIClient.client())
   defp account_id, do: Application.get_env(:docusign, :user_id)
 end
